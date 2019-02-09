84bb28836508c6143b9c2c3094607994667dd0ba
Debjyoti Pandit <debjyotipandit35@gmail.com>
2019-02-09 21:13:13 +0530

    Demo application to test whether angular in node

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const
+  express = require('express'),
+  http = require('http'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
+require('dotenv').config();
+
+app.use(express.static(__dirname+'/dist/frontend'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname));
+});
+
+http.createServer(app).listen(process.env.PORT || 1337, () => console.log('Your server is listening in port: ' + process.env.PORT || 1337));
